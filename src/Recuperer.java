a24c63fc97a7f1ea7d25d0810e7f42f3be757492
Hernandez Francois <francois.hernandez@supelec.fr>
2016-11-17 11:01:21 +0100

    Recuperer

---
+++ src/Recuperer.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Recuperer extends Generateur {
+
+	Recuperer(){}
+
+	@Override
+	Instance generer(int n, int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	ArrayList<Instance> getFromFile(String name) throws IOException{
+		FileReader fr = new FileReader("instances/"+name);
+		BufferedReader br = new BufferedReader(fr);
+
+		String firstline = br.readLine();
+
+		int nbTests = Integer.parseInt(firstline);
+		System.out.println(nbTests);
+
+		String empty = br.readLine();
+
+		ArrayList<Integer> nbInstances = new ArrayList<Integer>();
+		ArrayList<Integer> nbCentres = new ArrayList<Integer>();
+		ArrayList<Instance> instances = new ArrayList<Instance>();
+
+		for (int i = 0; i<nbTests;i++){
+			String[] integersInString = br.readLine().split(",");
+			int a[] = new int[integersInString.length];
+			for (int j = 0; j< integersInString.length; j++){
+				a[j] = Integer.parseInt(integersInString[j]);
+			}
+			integersInString = br.readLine().split(",");
+			int b[] = new int[integersInString.length];
+			for (int j = 0; j< integersInString.length; j++){
+				b[j] = Integer.parseInt(integersInString[j]);
+				System.out.println(b[j]);
+			}
+			ArrayList<Point> points = new ArrayList<Point>();
+			for (int k = 0; k<b.length; k=k+2){
+				Point p = new Point(b[k],b[k+1]);
+				System.out.println(b[k]);
+				System.out.println(b[k+1]);
+				points.add(p);
+			}
+			Instance instance = new Instance(a[0],a[1],points);
+			instances.add(instance);
+			empty = br.readLine();
+		}
+
+		return instances;
+
+	}
+	
+	void createFile(ArrayList<Instance> instances){
+		
+	}
+
+}
